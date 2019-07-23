d222b87d02c9dd3b2e497c5235530e89ae9318e6
Rayden <rb.ridene@gmail.com>
2019-07-23 22:33:32 +0200

    remove router (#2)

--- web/src/index.js
+++ web/src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom'
 import Main from './app/Main'
 import {history, store} from "./config/store";
 import { Provider } from 'react-redux';
-import { Router } from 'react-router-dom';
 
 ReactDOM.render(
     <Provider store={store}>
-      <Router history={history}>
         <Main />
-      </Router>
     </Provider>,
   document.getElementById('app')
 )
